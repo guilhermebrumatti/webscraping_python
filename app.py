553266d4f13eae32813054cf7661f82c146b62bc
guilhermebrumatti <guilhermegnb@hotmail.com>
2022-09-13 21:11:47 -0300

    Divisão da coluna Time e 2, Time e UF. Divisão da coluna Rescentes, adotando 0s para indicar Falso e 1 para Verdadeiro.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from array import array
 from encodings.utf_8 import decode
+from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import requests
 
@@ -20,12 +22,26 @@ df.drop("Próx", axis = 1, inplace=True)
 df.drop("%", axis = 1, inplace=True)
 
 #dividindo a coluna posição em 3: Ranking, variação de posição e nome do time
-posi = df["Posição"].str.split("  ", n = 3, expand=True)
-df.insert(column='Ranking', value=posi[0], loc=0)
-df.insert(column='Variação de Posição', value=posi[1], loc=1)
-df.insert(column='Time', value=posi[2], loc=2)
-#deletando a coluna Posição, original
-del df['Posição']
+posi_results = df["Posição"].str.extract('(?P<ranking>[1-91020º]+){1}\s*(?P<variacao>[0-9+-]+){1}\s*(?P<time>[A-Za-záãéí\s*]+){1}\s*(?P<uf>(?<=\s\-\s)[A-Z]+){2}', expand=True)
+df.insert(column="Ranking", value=posi_results["ranking"], loc=df.shape[1])
+df.insert(column="Variação de posição", value=posi_results["variacao"], loc=df.shape[1])
+df.insert(column="Time", value=posi_results["time"], loc=df.shape[1])
+df.insert(column="UF", value=posi_results["uf"], loc=df.shape[1])
+
+del df["Posição"]
+
+#dividindo a coluna Recentes em 6: Ultimo jogo - vitoria, ultimo jogo - empate, ultimo jogo - derrota.
+recent_results = df["Recentes"].str.extract('(?P<antepunultima>[A-Z]{1})\s*(?P<penultima>[A-Z]{1})\s*(?P<ultima>[A-Z]{1})', expand=True)
+df.insert(column="Antepenúltimo", value=recent_results["antepunultima"], loc=df.shape[1])
+df.insert(column="Penúltimo", value=recent_results["penultima"], loc=df.shape[1])
+df.insert(column="Último", value=recent_results["ultima"], loc=df.shape[1])
+
+#  remove a coluna 'Recentes'
+del df["Recentes"]
+
+# aplicamos a função get_dummies para converter as colunas categóricas em variáveis dummy
+# https://pandas.pydata.org/docs/reference/api/pandas.get_dummies.html
+df = pd.get_dummies(df, columns=["Antepenúltimo", "Penúltimo", "Último"])
 
 print(df)
 
